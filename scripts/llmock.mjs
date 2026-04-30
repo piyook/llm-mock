fc515047f1c079b56a5cd4ba4d1c6588ba393bc9
piyook <piyook@piyook.com>
2026-04-30 16:44:30 +0100

    fix(): use local bin script in llmock.mjs

--- scripts/llmock.mjs
+++ scripts/llmock.mjs
@@ -2,8 +2,8 @@ import { execSync } from 'child_process';
 
 const args = process.argv.slice(2).join(' ');
 
-console.log('🔨 Compiling UI...');
+console.log(' Compiling UI...');
 execSync('npm run compile-ui', { stdio: 'inherit' });
 
-console.log('🚀 Starting llmock...');
-execSync(`llmock ${args}`, { stdio: 'inherit' });
+console.log(' Starting llmock...');
+execSync(`node ./bin/llmock.js ${args}`, { stdio: 'inherit' });
